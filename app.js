443e3b1713fcd56c791517403819500a3edb7b89
bekuh-esfira <bekbolsun69.69@gmail.com>
2024-11-04 12:02:59 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+// console.log(helloo)
+// console.error()
+// var, let, const
+// var age =60;
+// console.log(age)
+
+// var age =20;
+// console.log(work)
+
+// var age =1;
+// console.log(work)
+
+// let hobbi = "cooking"
+
+// console.log(form)
+// var age =14;
+// console.log(age)
+//  let cook ="cooking";
+//  console.log(cook)
+// const car ="zeekr"
+// console.log(car)
+// let text = 'менин жашым '
+// let age = 18; 
+//  console.log(text)
+//  console.log(age)
+// let result = text+age;
+
+
+// let name ="max"
+// let old =12;
+// console.log(` менин атым` + name + " менин жышм " + 
+//  let name = "   Bekbolsun";
+// let job =' programer'
+// console.log(   `менин атым ` + name + job )
+
+  
+
+
+
+//  const age =prompt("сен кайсы жакта жашайсын");
+//  prompt("сен кайсы жакта жашайсын");
+
+
+
+//  const a =prompt("сан жаз")
+// const v =prompt("сан жаз")
+
+
+prompt("сен канча бир тугансын?")
+prompt("аты ким?")
+prompt("аты ким?")
+prompt("аты ким?")
+ prompt("аты ким?")
+let name = " bekbolsun, Aslan, Ainazik, begimai. "
+
+console.log(`Мен 4 бир тууган мын ` + name)
+
+
+
+
+
